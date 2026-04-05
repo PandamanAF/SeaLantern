6565cbe39b61b2d3f97ae3b47ebff8013ba6e7a0
PandamanAF <166897974+PandamanAF@users.noreply.github.com>
2026-04-05 19:55:23 +0800

    fix: 核心版本选择
    该commit用于过commitline审查

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -134,7 +134,7 @@ export const contributors: Contributor[] = [
   },
   {
     name: "欧耶熊猫人",
-    role: "Github文档转英文",
+    role: "Doc翻译&一点code",
     avatar: "https://api.rms.net.cn/head/Pandaman_AF",
     url: {
       github: "https://github.com/PandamanAF",
